2c3364532698cb63ae94af0223a188a82525c0b7
Ivan Dikic <ivan.dikic@futurice.com>
2015-06-07 11:26:39 +0200

    Bind UI to the BL. Add 'debug' mode. Add NSTask notification. Add 'deinit'.

--- youtube-dl/03_VIEW/MainWindowController.swift
+++ youtube-dl/03_VIEW/MainWindowController.swift
@@ -18,20 +18,36 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var downloadURLTextField: NSTextField!
     @IBOutlet weak var downloadPathControl: NSPathControl!
     @IBOutlet var outputTextView: NSTextView!
-    @IBOutlet weak var cancelButton: NSButton!
+    @IBOutlet weak var cancelButton: NSButton! {
+        didSet {
+            cancelButton.enabled = false
+        }
+    }
     @IBOutlet weak var downloadButton: NSButton!
-    @IBOutlet weak var progressIndicator: NSProgressIndicator!
+    @IBOutlet weak var progressIndicator: NSProgressIndicator! {
+        didSet {
+            progressIndicator.hidden = true
+        }
+    }
 
     // MARK - Propertys
     var isRunning = false
     var runningTask: NSTask?
+    var debug = true
 
     // MARK - Lifecycle
     override func windowDidLoad() {
         super.windowDidLoad()
 
-        // Implement this method to handle any initialization 
-        // after your window controller's window has been loaded from its nib file.
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self,
+                                        selector: "updateUI",
+                                        name: NSTaskDidTerminateNotification,
+                                        object: nil)
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     // MARK - User Actions
@@ -44,17 +60,20 @@ class MainWindowController: NSWindowController {
     @IBAction func onDownloadButtonPress(sender: NSButton) {
 
         var taskQueue: dispatch_queue_t = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
-        dispatch_async(taskQueue) {
 
+        dispatch_async(taskQueue) {
             var downloadPath = self.downloadURLTextField.stringValue
             var outputPath = self.downloadPathControl.URL?.path
             var arguments = [Parameters.Verbose.rawValue, downloadPath]
             var binaryPath = "/usr/local/bin/youtube-dl"
 
-            println("Arguments: \(arguments)\nBinary Path:\(binaryPath)")
+            if self.debug {
+                println("\nArguments: \(arguments)\nBinary Path:\(binaryPath)\n")
+            }
 
             self.runningTask = self.createTask(binaryPath, arguments: arguments, outputPath: outputPath)
             self.runningTask!.launch()
+            self.updateUI()
             self.runningTask!.waitUntilExit()
         }
     }
@@ -65,20 +84,29 @@ class MainWindowController: NSWindowController {
         task.currentDirectoryPath = outputPath ?? "~/Downloads"
         task.launchPath = pathToBinary
         task.arguments = arguments
+
+        if debug {
+            println("\nCurrent directory path: \(task.currentDirectoryPath)\n")
+        }
+
         return task
     }
 
     func updateUI() {
-        if isRunning {
+        if !isRunning {
             downloadButton.enabled = false
             cancelButton.enabled = true
             progressIndicator.startAnimation(self)
+            progressIndicator.hidden = false
             outputTextView.string = ""
         } else {
             downloadButton.enabled = true
             cancelButton.enabled = false
             progressIndicator.stopAnimation(self)
+            progressIndicator.hidden = true
         }
+
+        isRunning = !isRunning
     }
 
 }
